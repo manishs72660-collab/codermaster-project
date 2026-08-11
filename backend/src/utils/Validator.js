8c4fc3893fb00dd9b03b8bbca5900fc5a7ccd536
manish kumar <manishs72660@gmail.com>
2026-08-11 22:38:04 +0530

    fix some bug

--- backend/src/utils/Validator.js
+++ backend/src/utils/Validator.js
@@ -5,15 +5,16 @@ const mandatoryField = ["firstName","emailId","password"]
         const IsAllowed = mandatoryField.every((k)=> Object.keys(data).includes(k));
         if(!IsAllowed)
             throw new Error("Fields Missing");
+        if(!validator.isEmail(data.emailId))
+            throw new Error("Invalid Email");
+        if(!(data.firstName.length>=3 && data.firstName.length<=20))
+            throw new Error("Name should have atleast 3 char and atmost 20 char");
         const {emailId}=data;
         const isexit=await User.exists({ emailId });
         if(isexit){
             throw new Error("user user already exit");
         }
-        if(!validator.isEmail(data.emailId))
-            throw new Error("Invalid Email");
-        if(!(data.firstName.length>=3 && data.firstName.length<=20))
-            throw new Error("Name should have atleast 3 char and atmost 20 char");
+       
 };
 
 module.exports = validUser;
