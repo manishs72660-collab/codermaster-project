ab6444fec4200b3c22e26e9cc62fb45066f24040
manish kumar <manishs72660@gmail.com>
2026-08-27 14:12:21 +0530

    logo fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router";
-import {store} from "./store/store.js"
+import { store } from "./store/store.js"
 import { Provider } from 'react-redux';
-
+import logo from './assets/mylogo.png'
+document.head.insertAdjacentHTML(
+  'beforeend',
+  `<link rel="icon" type="image/png" href="${logo}" />`
+);
+document.title = "CodeMaster";
 createRoot(document.getElementById('root')).render(
     <Provider store={store}>
     <BrowserRouter>
